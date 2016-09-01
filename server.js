fe3165f509d2270206098f601d0bd39f521bedfa
Eytyy <e.tayyem@gmail.com>
2016-09-01 18:23:13 +0300

    testing aws

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const glob = require('glob');
+var AWS = require('aws-sdk');
 
 const port = process.env.PORT || 3000;
 const router = express.Router();
@@ -14,31 +15,43 @@ app.get('/', function(req, res) {
   res.end();
 });
 app.get('/resources/images/:layer/:id', function(req, res) {
-  const dir__name = './app'
-  const path = `http://s3.amazonaws.com/eytyy.com/welfare/${req.params.layer}/${req.params.id}/Photo`;
-  const fullpath =  `${path}`;
-
   let results = [];
-
-  function sendResponse(results) {
-    res.json({ data: results});
-    res.end();
-  }
-
-  fs.readdir(fullpath, function(err, data) {
-    // If direcotry doesn't exist
+  const s3 = new AWS.s3();
+  const params = {
+    Bucket: process.env.S3_BUCKET_NAME,
+    Delimiter: '/',
+    Prefix: `welfare/${req.params.layer}/${req.params.id}/Photo`
+  };
+  s3.listObjects(params, function(err, data) {
     if (err) {
       console.error('There was an error reading the file!', err);
-      sendResponse();
-      return;
     }
-    // Otherwise handle data
-    data.forEach(function(file) {
-      file = path + '/' + file;
-      results.push(file);
-    });
-    sendResponse(results);
+    console.log(data);
   });
+
+  // const dir__name = './app'
+  // const path = `http://s3.amazonaws.com/eytyy.com/welfare/${req.params.layer}/${req.params.id}/Photo`;
+  // const fullpath =  `${path}`;
+
+  // function sendResponse(results) {
+  //   res.json({ data: results});
+  //   res.end();
+  // }
+
+  // fs.readdir(fullpath, function(err, data) {
+  //   // If direcotry doesn't exist
+  //   if (err) {
+  //     console.error('There was an error reading the file!', err);
+  //     sendResponse();
+  //     return;
+  //   }
+  //   // Otherwise handle data
+  //   data.forEach(function(file) {
+  //     file = path + '/' + file;
+  //     results.push(file);
+  //   });
+  //   sendResponse(results);
+  // });
 });
 app.get('/resources/other/:layer/:id', function(req, res) {
   // const dir__name = './app'
