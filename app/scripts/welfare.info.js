02d698ef15ed3130294a6bc49061478d9bf546f4
Eytyy <e.tayyem@gmail.com>
2016-09-04 04:21:25 +0300

    fixes

--- app/scripts/welfare.info.js
+++ app/scripts/welfare.info.js
@@ -149,8 +149,8 @@ welfare.info = function () {
         miscImages: [],
         otherFiles: []
       };
-      console.log('images');
-      console.log(allData[0]);
+      // console.log('images');
+      // console.log(allData[0]);
       allData[0].forEach(function (item, index) {
         if (/before.jpg/i.test(item)) {
           obj.beforeImage = item;
@@ -165,8 +165,8 @@ welfare.info = function () {
         }
       });
 
-      console.log('links');
-      console.log(allData[1]);
+      // console.log('links');
+      // console.log(allData[1]);
       allData[1].forEach(function (item, index) {
         if (index !== 0) {
           obj.otherFiles.push(item);
--- src/scripts/welfare.info.js
+++ src/scripts/welfare.info.js
@@ -143,8 +143,8 @@ welfare.info = (() => {
         miscImages: [],
         otherFiles: [],
       };
-      console.log('images');
-      console.log(allData[0]);
+      // console.log('images');
+      // console.log(allData[0]);
       allData[0].forEach((item, index) => {
         if (/before.jpg/i.test(item)) {
           obj.beforeImage = item;
@@ -162,8 +162,8 @@ welfare.info = (() => {
         }
       });
 
-      console.log('links');
-      console.log(allData[1]);
+      // console.log('links');
+      // console.log(allData[1]);
       allData[1].forEach((item, index) => {
         if (index !== 0) {
           obj.otherFiles.push(item);
